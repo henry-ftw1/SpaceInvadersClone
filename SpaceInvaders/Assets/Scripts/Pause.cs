b24258db20a001b61127f75c895f5b99ff5b2a7c
Brad <brad.dela.cruz@icloud.com>
2019-02-08 15:05:18 -0800

    added Ui stuff

---
+++ SpaceInvaders/Assets/Scripts/Pause.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject menu;
+    public bool paused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Stop();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        paused = false;
+        menu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    void Stop()
+    {
+        paused = true;
+        Time.timeScale = 0f;
+        menu.SetActive(true);
+    }
+}
