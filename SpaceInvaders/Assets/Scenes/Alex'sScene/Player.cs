9f1e591223b7a6dcb84f13cb79642e8f9343b82d
Alex Koufakis <akoufaki@uci.edu>
2019-02-07 18:39:15 -0800

    Put in the basics of the player: movement and shooting

---
+++ SpaceInvaders/Assets/Scenes/Alex'sScene/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int lives = 3;
+    public int speed = 3;
+    public GameObject player_projectile;
+    private Rigidbody2D player_rigidbody;
+
+    void Start()
+    {
+        player_rigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+
+        if ((Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Space)) && !(GameObject.Find("PlayerProjectile(Clone)")))
+        {
+            shoot();
+        }
+    }
+
+    void move()
+    {
+        float moveModifier = Input.GetAxis("Horizontal");
+        Vector2 currentVelocity = player_rigidbody.velocity;
+        player_rigidbody.velocity = new Vector2(moveModifier * speed, currentVelocity.y);
+    }
+
+    void shoot()
+    {
+        Vector2 here = (Vector2)this.transform.position;
+        Instantiate(player_projectile, here, Quaternion.identity);
+    }
+}
