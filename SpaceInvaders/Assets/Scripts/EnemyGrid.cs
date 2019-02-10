08df4decc94e397fcef3b0c87a55211906072208
Henry <cartoonhenry@yahoo.com>
2019-02-10 00:50:53 -0800

    Added Enemy and Wall to the prefabs under Main

--- SpaceInvaders/Assets/Scripts/EnemyGrid.cs
+++ SpaceInvaders/Assets/Scripts/EnemyGrid.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class EnemyGrid : MonoBehaviour
 {
-    EnemyMovement enemyComponent;
-    //Go into each child and set their direction to move to the opposite direction if an event is triggered.
-    void Start()
-    {
-        enemyComponent = GameObject.Find("Enemy").GetComponent<EnemyMovement>();
-        enemyComponent.hitWallEvent += OnHitWallEvent;
-    }
+    //EnemyMovement enemyComponent;
+    ////Go into each child and set their direction to move to the opposite direction if an event is triggered.
+    //void Start()
+    //{
+    //    enemyComponent = GameObject.Find("Enemy").GetComponent<EnemyMovement>();
+    //    enemyComponent.hitWallEvent += OnHitWallEvent;
+    //}
 
-    void OnHitWallEvent()
-    {
-        Debug.Log("Triggered!");
-        bool enemyMR = this.transform.Find("Enemy").GetComponent<EnemyMovement>().moveRight;
-        if (enemyMR)
-            enemyMR = false;
-        else
-            enemyMR = true;
-        //enemyMR = !enemyMR;
-    }
+    //void OnHitWallEvent()
+    //{
+    //    int childCount = this.transform.childCount;
+    //    for (int i = 1; i <= childCount; i++)
+    //    {
+    //        if (i == 1)
+    //            this.transform.Find("Enemy").GetComponent<EnemyMovement>().moveRight = !this.transform.Find("Enemy").GetComponent<EnemyMovement>().moveRight;
+    //        else
+    //        {
+    //            string ECount = string.Format("Enemy ({0})", i);
+    //            Debug.LogFormat("Triggered! {0}", ECount);
+    //            this.transform.Find(ECount).GetComponent<EnemyMovement>().moveRight = !this.transform.Find(ECount).GetComponent<EnemyMovement>().moveRight;
+    //        }
+    //    }
+    //    //Debug.Log("Triggered!");
+    //    //this.transform.Find("Enemy").GetComponent<EnemyMovement>().moveRight = !this.transform.Find("Enemy").GetComponent<EnemyMovement>().moveRight;
+    //}
 }
--- SpaceInvaders/Assets/Scripts/EnemyMovement.cs
+++ SpaceInvaders/Assets/Scripts/EnemyMovement.cs
@@ -6,8 +6,11 @@ using UnityEngine.Events;
 public class EnemyMovement : MonoBehaviour
 {
     public float moveSpeed;
-    public float moveDown;
-    public bool moveRight;
+    private float moveDown;
+    private bool moveRight;
+
+    public Rigidbody2D m_rb;
+    EnemyMovement enemyComponent;
 
     public delegate void hitWallDelegate();
     public event hitWallDelegate hitWallEvent = delegate { };
@@ -19,26 +22,36 @@ public class EnemyMovement : MonoBehaviour
         moveSpeed = 10f;
         moveDown = 0.1f;
         moveRight = true;
+        m_rb = this.transform.GetComponent<Rigidbody2D>();
+        enemyComponent = GameObject.Find("Enemy").GetComponent<EnemyMovement>();
+        enemyComponent.hitWallEvent += OnHitWallEvent;
     }
     void Start()
     {
     }
 
     // Update is called once per frame
-    void Update()
+    //void Update()
+    //{
+    //    Move();
+    //}
+
+    void FixedUpdate()
     {
         Move();
     }
 
     void Move()
     {
-        if (moveRight)
+        if(moveRight)
         {
+            //m_rb.AddForce(Vector2.right);
             this.transform.Translate(Vector2.right * Time.deltaTime * moveSpeed);
         }
             
         else
         {
+            //m_rb.AddForce(Vector2.left);
             this.transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
         }
     }
@@ -49,10 +62,16 @@ public class EnemyMovement : MonoBehaviour
     {
         if (other.collider.CompareTag("Wall"))
         {
-            //this.transform.Translate(Vector2.down * moveDown);
+            this.transform.Translate(Vector2.down * moveDown);
             hitWallEvent();
             //Debug.Log("Hit Collider");
             //moveRight = !moveRight;
         }
+    }
+
+    void OnHitWallEvent()
+    {
+        Debug.Log("Hit Wall Event Triggered");
+        this.moveRight = !this.moveRight;
     }
 }
