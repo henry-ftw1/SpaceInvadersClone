c05c947a6d0d26397554ca40dd082cefd94df394
Henry <henryhernandez01998@gmail.com>
2019-02-11 12:33:04 -0800

    Updated Enemy Movement

--- SpaceInvaders/Assets/Scripts/EnemyGrid.cs
+++ SpaceInvaders/Assets/Scripts/EnemyGrid.cs
@@ -4,29 +4,35 @@ using UnityEngine;
 
 public class EnemyGrid : MonoBehaviour
 {
-    //EnemyMovement enemyComponent;
-    ////Go into each child and set their direction to move to the opposite direction if an event is triggered.
-    //void Start()
-    //{
-    //    enemyComponent = GameObject.Find("Enemy").GetComponent<EnemyMovement>();
-    //    enemyComponent.hitWallEvent += OnHitWallEvent;
-    //}
+    EnemyMovement enemyComponent;
+    public bool moveR;
 
-    //void OnHitWallEvent()
-    //{
-    //    int childCount = this.transform.childCount;
-    //    for (int i = 1; i <= childCount; i++)
-    //    {
-    //        if (i == 1)
-    //            this.transform.Find("Enemy").GetComponent<EnemyMovement>().moveRight = !this.transform.Find("Enemy").GetComponent<EnemyMovement>().moveRight;
-    //        else
-    //        {
-    //            string ECount = string.Format("Enemy ({0})", i);
-    //            Debug.LogFormat("Triggered! {0}", ECount);
-    //            this.transform.Find(ECount).GetComponent<EnemyMovement>().moveRight = !this.transform.Find(ECount).GetComponent<EnemyMovement>().moveRight;
-    //        }
-    //    }
-    //    //Debug.Log("Triggered!");
-    //    //this.transform.Find("Enemy").GetComponent<EnemyMovement>().moveRight = !this.transform.Find("Enemy").GetComponent<EnemyMovement>().moveRight;
-    //}
+    private void Awake()
+    {
+        moveR = true;
+    }
+
+    //Go into each child and set their direction to move to the opposite direction if an event is triggered.
+    void Start()
+    {
+        int childCount = this.gameObject.transform.childCount;
+        for (int i = 0; i < childCount; i++)
+        {
+            enemyComponent = this.gameObject.transform.GetChild(i).GetComponent<EnemyMovement>();
+            enemyComponent.hitWallEvent += OnHitWallEvent;
+        }
+    }
+
+    void OnHitWallEvent()
+    {
+        Debug.Log("Event Was Hit");
+        moveR = !moveR;
+        int childCount = this.gameObject.transform.childCount;
+        for (int i = 0; i < childCount; i++)
+        {
+            EnemyMovement enemy = this.gameObject.transform.GetChild(i).GetComponent<EnemyMovement>();
+            enemy.MoveDown();
+            enemy.Move();
+        }
+    }
 }
--- SpaceInvaders/Assets/Scripts/EnemyMovement.cs
+++ SpaceInvaders/Assets/Scripts/EnemyMovement.cs
@@ -7,12 +7,9 @@ public class EnemyMovement : MonoBehaviour
 {
     public float moveSpeed;
     private float moveDown;
-    private bool moveRight;
+    public int points;
 
     public GameObject enemy_projectile;
-    public Rigidbody2D m_rb;
-    private Vector2 m_position;
-    EnemyMovement enemyComponent;
 
     public delegate void hitWallDelegate();
     public event hitWallDelegate hitWallEvent = delegate { };
@@ -23,42 +20,39 @@ public class EnemyMovement : MonoBehaviour
     {
         moveSpeed = 5f;
         moveDown = 0.1f;
-        moveRight = true;
-        m_rb = this.transform.GetComponent<Rigidbody2D>();
-        m_position = (Vector2)this.transform.position;
-        enemyComponent = GameObject.Find("Enemy").GetComponent<EnemyMovement>();
-        enemyComponent.hitWallEvent += OnHitWallEvent;
+        points = 10;
     }
     void Start()
     {
-        m_rb.velocity = new Vector2(moveSpeed, 0);
+        //m_rb.velocity = new Vector2(moveSpeed, 0);
+        //this.transform.Translate(Vector2.right * Time.deltaTime * moveSpeed);
     }
 
     //Update is called once per frame
     void Update()
     {
         Move();
-
         if (!(GameObject.Find("EnemyProjectile(Clone)")))
         {
             shoot();
         }
     }
 
-    void Move()
+    public void Move()
     {
-        if(moveRight)
+        if(this.transform.parent.GetComponentInParent<EnemyGrid>().moveR)
         {
-            ////m_rb.AddForce(Vector2.right);
-            //this.transform.Translate(Vector2.right * Time.deltaTime * moveSpeed);
-            m_position = new Vector2(m_position.x + 1, 0);
+            this.transform.Translate(Vector2.right * Time.deltaTime * moveSpeed);
         }
         else
         {
-            ////m_rb.AddForce(Vector2.left);
-            //this.transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
-            m_position = new Vector2(m_position.x - 1, 0);
+            this.transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
         }
+    }
+
+    public void MoveDown()
+    {
+        this.transform.Translate(Vector2.down * moveDown);
     }
 
     void shoot()
@@ -70,12 +64,7 @@ public class EnemyMovement : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.collider.CompareTag("Wall"))
-        {
-            this.transform.Translate(Vector2.down * moveDown);
-            OnHitWallEvent();
-            //Debug.Log("Hit Collider");
-            //moveRight = !moveRight;
-        }
+            hitWallEvent();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -84,15 +73,5 @@ public class EnemyMovement : MonoBehaviour
         {
             Destroy(gameObject);
         }
-    }
-
-
-    void OnHitWallEvent()
-    {
-        Debug.Log("Hit Wall Event Triggered");
-        moveRight = !moveRight;
-        moveSpeed = -moveSpeed;
-        m_rb.velocity = new Vector2(moveSpeed, 0);
-        this.transform.Translate(Vector2.down * moveDown);
     }
 }
